83feef92ea21e7b389475336a0f1d61e0db5509a
Espen Limi <Espen.Limi@aenergi.no>
2024-08-23 11:54:07 +0200

    Add project files.

---
+++ Kartverk.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Kartverk.Mvc.Models;
+
+namespace Kartverk.Mvc.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        var model = new HomeViewModel();
+        model.Message = "Det tar en time å gå ned til Ørsta rådhus";
+        
+        return View("NotIndex",model);
+       
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Kartverk.Mvc/Models/HomeViewModel.cs
@@ -0,0 +1,4 @@
+public class HomeViewModel
+{
+    public string Message { get; set; }
+}
