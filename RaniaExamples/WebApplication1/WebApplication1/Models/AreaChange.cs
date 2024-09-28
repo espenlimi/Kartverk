d52100037957002c4abad5ffbf7f523b1c7c64c0
RaniaEG <79601775+RaniaEG@users.noreply.github.com>
2024-09-29 00:52:47 +0200

    Create WebApplication1

---
+++ RaniaExamples/WebApplication1/WebApplication1/Models/AreaChange.cs
@@ -0,0 +1,11 @@
+﻿
+namespace WebApplication1.Models
+{
+    public class AreaChange
+    {
+        public string Id { get; set; }
+        public string GeoJson { get; set; }  // GeoJSON format for points, lines, or polygons
+        public string Description { get; set; }
+
+    }
+}
